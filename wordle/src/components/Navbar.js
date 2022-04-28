e07fc4521897bfaa24b26328a3eb10b90677ba4f
abhayp2204 <abhaypatil1020@gmail.com>
2022-04-28 23:21:40 +0530

    navbar

---
+++ wordle/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "../css/Navbar.css"
+
+function Navbar() {
+    return (
+    <div className="navbar">
+        Wordle
+    </div>
+    )
+}
+    
+export default Navbar
